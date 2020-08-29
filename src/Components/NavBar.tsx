fad64f9d6b953ac4ff602424d62f44fa458f10e1
YashTotale <totaleyash@gmail.com>
2020-08-29 14:43:00 -0700

    navbar btns working

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -9,23 +9,21 @@ import { toggleDarkMode } from "../redux/actions";
 
 //Material UI Imports
 import { makeStyles, Theme } from "@material-ui/core/styles";
-import { Brightness7, Brightness4 } from "@material-ui/icons";
+import { Brightness7, Brightness4, GitHub } from "@material-ui/icons";
 import {
   AppBar,
   Toolbar,
   Tabs,
   Tab,
   TabProps,
-  Button,
   IconButton,
   Tooltip,
+  IconButtonProps,
 } from "@material-ui/core";
+
 const useStyles = makeStyles((theme: Theme) => ({
   root: {
     flexGrow: 1,
-  },
-  toolBar: {
-    justifyContent: "center",
   },
   tabs: {
     "& div": {
@@ -34,6 +32,44 @@ const useStyles = makeStyles((theme: Theme) => ({
     flexGrow: 1,
   },
 }));
+
+export const NavBar: React.FC = (props) => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  const isDarkMode = useSelector(getIsDarkMode);
+
+  const btns = [
+    {
+      title: `${isDarkMode ? "Light" : "Dark"} Theme`,
+      onClick: () => dispatch(toggleDarkMode()),
+      icon: isDarkMode ? <Brightness7 /> : <Brightness4 />,
+    },
+    {
+      title: "GitHub Repository",
+      icon: <GitHub />,
+      href: "http://github.com/YashTotale/YashTotale.github.io",
+    },
+  ];
+
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+  const tabs = ["home", "projects", "experience"];
+  const currentTab = tabs.includes(path) ? path : "home";
+
+  return (
+    <div className={classes.root}>
+      <AppBar elevation={2} color="transparent" position="static">
+        <Toolbar className={classes.toolBar}>
+          <Tabs className={classes.tabs} value={currentTab}>
+            {tabs.map(createTab)}
+          </Tabs>
+          {btns.map(NavButton)}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+};
 
 const createTab = (tab: string): TabProps => {
   const upperCase = tab.toUpperCase();
@@ -48,34 +84,28 @@ const createTab = (tab: string): TabProps => {
   );
 };
 
-export const NavBar: React.FC = (props) => {
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const location = useLocation();
-  const isDarkMode = useSelector(getIsDarkMode);
-  const path = location.pathname.split("/")[1];
+interface INavButton {
+  title: string;
+  icon: IconButtonProps;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => any;
+  href?: string;
+}
 
-  const tabs = ["home", "projects", "experience"];
-
-  const currentTab = tabs.includes(path) ? path : "home";
-
+const NavButton: React.FC<INavButton> = ({ title, icon, onClick, href }) => {
   return (
-    <div className={classes.root}>
-      <AppBar elevation={2} color="transparent" position="static">
-        <Toolbar className={classes.toolBar}>
-          <Tabs className={classes.tabs} value={currentTab}>
-            {tabs.map(createTab)}
-          </Tabs>
-          <Tooltip title={`${isDarkMode ? "Light" : "Dark"} Theme`}>
-            <IconButton
-              title="Toggle Theme"
-              onClick={(e) => dispatch(toggleDarkMode())}
-            >
-              {isDarkMode ? <Brightness7 /> : <Brightness4 />}
-            </IconButton>
-          </Tooltip>
-        </Toolbar>
-      </AppBar>
-    </div>
+    <Tooltip key={title} title={title}>
+      {href ? (
+        <IconButton
+          component="a"
+          target="_blank"
+          rel="noopener noreferrer"
+          href={href}
+        >
+          {icon}
+        </IconButton>
+      ) : (
+        <IconButton onClick={onClick}>{icon}</IconButton>
+      )}
+    </Tooltip>
   );
 };
