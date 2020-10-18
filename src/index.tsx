afade2f0f5548e47d28953e4a33387b06916309a
YashTotale <totaleyash@gmail.com>
2020-10-17 17:19:58 -0700

    registered service worker

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,7 @@ import App from "./App";
 import ReduxStore from "./Redux/store";
 
 //Service Worker Imports
-import { register, unregister } from "./serviceWorker";
+import { register } from "./serviceWorker";
 
 ReactDOM.render(
   <ReduxStore>
@@ -16,7 +16,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-unregister();
+register();
