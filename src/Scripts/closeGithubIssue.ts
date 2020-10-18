2876a487668d254936e3e8a839efbe2dc7ff4f54
YashTotale <totaleyash@gmail.com>
2020-10-17 22:09:54 -0700

    fixed build error

--- src/Scripts/closeGithubIssue.ts
+++ src/Scripts/closeGithubIssue.ts
@@ -1,56 +1,25 @@
-import fetch from "node-fetch";
+import { Octokit } from "@octokit/rest";
 
 const args = process.argv;
 
 export const closeGithubIssue = async (githubToken: string) => {
-  const issueResponse = await getIssueData(githubToken);
-  if (!issueResponse.ok) process.exit(1);
+  const octockit = new Octokit({
+    auth: githubToken,
+    userAgent: "YashTotale",
+  });
 
-  const issueData = await issueResponse.json();
-
-  if (issueData.state === "open") {
-    const changeSuccess = await changeIssueToClosed(githubToken);
-    if (changeSuccess) {
-      console.log("Issue has been closed");
-      process.exit(0);
-    }
+  try {
+    await octockit.issues.update({
+      owner: "YashTotale",
+      repo: "YashTotale.github.io",
+      issue_number: 4,
+      state: "closed",
+    });
+  } catch (e) {
+    console.log(e);
     process.exit(1);
-  } else {
-    console.log("Issue is already closed");
   }
 };
-
-async function getIssueData(githubToken: string) {
-  const response = await fetch(
-    "https://api.github.com/repos/YashTotale/YashTotale.github.io/issues/4",
-    {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `token ${githubToken}`,
-      },
-    }
-  );
-  return response;
-}
-
-async function changeIssueToClosed(githubToken: string) {
-  const body = {
-    state: "closed",
-  };
-
-  const response = await fetch(
-    "https://api.github.com/repos/YashTotale/YashTotale.github.io/issues/4",
-    {
-      method: "PATCH",
-      body: JSON.stringify(body),
-      headers: {
-        Authorization: `token ${githubToken}`,
-      },
-    }
-  );
-  return response.status;
-}
 
 if (args.length < 3) {
   console.log("Please provide a GitHub OAuth Token");
